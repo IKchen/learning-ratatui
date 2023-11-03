4c75c4f265a8a0077b3f6d48df3ae1fced12986d
jim chen <kk.chenji@qq.com>
2023-11-03 19:31:45 +0800

    异步编程代码结构修改

--- src/main.rs
+++ src/main.rs
@@ -23,31 +23,16 @@ use update::update;
 use std::{io::Error,sync::mpsc::RecvError};
 use error::MyError;
 
-fn main() -> Result<(),MyError> {
-  // Create an application.
+async fn tokio_main()->Result<()>{
   let mut app = App::new();
-
-  // Initialize the terminal user interface.
-  let backend = CrosstermBackend::new(std::io::stdout());
-  let terminal = Terminal::new(backend)?;
-  let events = EventHandler::new(250);
-  let mut tui = Tui::new(terminal, events);
-  tui.enter()?;
-
-  // Start the main loop.
-  while !app.should_quit {
-    // Render the user interface.
-    tui.draw(&mut app)?;
-    // Handle events.
-    match tui.event.next()? {
-      Event::Tick => {},
-      Event::Key(key_event) => update(&mut app, key_event),
-      Event::Mouse(_) => {},
-      Event::Resize(_, _) => {},
-    };
-  }
-
-  // Exit the user interface.
-  tui.exit()?;
+  app.run().await?;
   Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(),MyError> {
+if let Err(e)=tokio_main.await{
+  eprintln!("{} error: Something went wrong", env!("CARGO_PKG_NAME"));
+  Err(e)
+} else{Ok(())}
 }
